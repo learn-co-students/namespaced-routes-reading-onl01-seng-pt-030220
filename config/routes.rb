636564178ca3d99f99e12006b19cd9321c838b43
alelee93 <leevargasale@gmail.com>
2020-08-02 19:08:15 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,14 @@ Rails.application.routes.draw do
   get '/admin/stats', to: 'stats#index'
 
   root 'posts#index'
+
+  # scope '/admin', module: 'admin' do
+  #   resources :stats, only: [:index]
+  # end
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
+  #get 'admin/stats', to: 'stats#index'
 end
